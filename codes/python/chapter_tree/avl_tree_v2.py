9bd772c45bbc47d27484434fb4ca4ec2c98ca040
huoyinghui <huoyinghui@xizhi-ai.com>
2025-01-14 11:46:15 +0800

    add: avl

--- codes/python/chapter_tree/avl_tree_v2.py
+++ codes/python/chapter_tree/avl_tree_v2.py
@@ -32,6 +32,7 @@ class AVLTree:
     我们将平衡因子绝对值>1的节点称为“失衡节点”。
     根据节点失衡情况的不同，旋转操作分为四种：右旋、左旋、先右旋后左旋、先左旋后右旋。
     """
+
     def height(self, node: TreeNode | None) -> int:
         """
         获取节点高度
@@ -43,6 +44,24 @@ class AVLTree:
         if node.is_leaf:
             return 0
         return node.height
+
+    def update_height(self, node: TreeNode | None) -> int:
+        """
+        节点高度等于最高子树高度 + 1
+        """
+        return max(self.height(node.left), self.height(node.right)) + 1
+
+    def balance_factor(self, node: TreeNode | None) -> int:
+        """
+        平衡因子（balance factor)
+        定义为节点左子树的高度减去右子树的高度，同时规定空节点的平衡因子为 0
+
+        失衡节点： 平衡因子绝对值>1, 需要旋转
+        """
+        if not node:
+            return 0
+        # 节点平衡因子 = 左子树高度 - 右子树高度
+        return self.height(node.left) - self.height(node.right)
 
     def right_rotate(self, node: TreeNode | None) -> TreeNode | None:
         pass
