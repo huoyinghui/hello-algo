438d101ed840e030b178a7bc04a7ec7d33ca1bd2
huoyinghui <huoyinghui@xizhi-ai.com>
2025-01-14 13:53:34 +0800

    add: left rotate

--- codes/python/chapter_tree/avl_tree_v2.py
+++ codes/python/chapter_tree/avl_tree_v2.py
@@ -96,6 +96,41 @@ class AVLTree:
         # 返回旋转后子树的根节点
         return child
 
+    def rotate(self, node: TreeNode | None) -> TreeNode | None:
+        """
+        执行旋转操作，使该子树重新恢复平衡
+        失衡节点的平衡因子	子节点的平衡因子	应采用的旋转方法
+        >1 (左偏)         >=0             右旋
+        >1 (左偏)         <0 (子节点，右偏) 先左旋后右旋
+
+        <-1 (右偏)        <=0              左旋
+        <-1 (右偏)        >0 (子节点，左偏) 先右旋后左旋
+        """
+        # 获取节点 node 的平衡因子
+        balance_factor = self.balance_factor(node)
+        # 左偏树
+        if balance_factor > 1:
+            if self.balance_factor(node.left) >= 0:
+                # 右旋
+                return self.right_rotate(node)
+            else:
+                # 先左旋后右旋
+                node.left = self.left_rotate(node.left)
+                return self.right_rotate(node)
+        elif balance_factor < 1:
+            # 右偏
+            if self.balance_factor(node.right) <= 0:
+                # 左旋
+                return self.left_rotate(node)
+            else:
+                # 先右旋后左旋
+                node.right = self.right_rotate(node.right)
+                return self.left_rotate(node)
+        else:
+            # 平衡树，无须旋转，直接返回
+            return node
+
+
 
 def main():
     # 初始化空 AVL 树
