97cd775d800e0382466e115e61ade1170572ef45
huoyinghui <huoyinghui@xizhi-ai.com>
2025-02-20 11:47:03 +0800

    update

--- codes/python/chapter_graph/demo_graph.py
+++ codes/python/chapter_graph/demo_graph.py
@@ -45,29 +45,117 @@ from typing import List, Tuple
 class GraphAdjMat:
     """
     邻接矩阵
+
+    边的操作： 值 1/0
+    顶点的操作: 行和列
     """
     def __init__(self, vertices: List[int] = None, edges: List[Tuple[int, int]] = None):
-        # 保存顶点
-        self.vertices = vertices
-        self.matrix = [[0 for _ in self.vertices] for _ in self.vertices]
-        # 保存边
-        self.edges = edges
-        for u_idx, v_idx in edges:
-            self.matrix[u_idx][v_idx] = 1
+        # 顶点列表，元素代表“顶点值”，索引代表“顶点索引”
+        self.vertices: list[int] = []
+        # 邻接矩阵，行列索引对应“顶点索引”
+        self.adj_mat: list[list[int]] = []
 
-    def __str__(self):
-        print(f"ver: {self.vertices}")
-        for row in self.matrix:
-            print(f"row: {row}")
-        return ""
+    def print(self):
+        """打印邻接矩阵"""
+        print("顶点列表 =", self.vertices)
+        print("邻接矩阵 =")
+        print(self.adj_mat)
+
+    def size(self) -> int:
+        """获取顶点数量"""
+        return len(self.vertices)
+
+    def add_node(self, val: int = 0):
+        """
+        n:0
+            0*0 []
+
+        n:1 1*1
+            [0]
+        n:2 2*2
+            先处理行1->2: 2*1
+            [0]
+            [0]
+            在处理列:     2*2
+            [0, 0]
+            [0, 0]
+        n:3
+            3x2 行处理 2->3
+            [0, 0]
+            [0, 0]
+            [0, 0]
+
+            3x3 列处理
+            [0, 0, 0]
+            [0, 0, 0]
+            [0, 0, 0]
+        """
+        n = self.size()
+        # 向顶点列表中添加新顶点的值
+        self.vertices.append(val)
+        # 在邻接矩阵中添加一列
+        new_row = [0] * n
+        self.adj_mat.append(new_row)
+
+        # 在邻接矩阵中添加一列
+        for row in self.adj_mat:
+            row.append(0)
+        return
+
+    def remove_node(self, val_idx: int = 0):
+        """
+        删除对应行，删除对应列
+        """
+        if val_idx not in self.vertices:
+            return False
+        # 通过下标删除
+        self.vertices.pop(val_idx)
+
+        # 在邻接矩阵中删除一行
+        # n*n -> (n-1)*n
+        self.adj_mat.pop(val_idx)
+
+        # 在邻接矩阵中删除一列
+        # (n-1)*n -> (n-1)*(n-1)
+        for row in self.adj_mat:
+            row.pop(val_idx)
+        return
+
+    def add_edge(self, i: int = 0, j: int = 0):
+        """添加边"""
+        # 参数 i, j 对应 vertices 元素索引
+        # 索引越界与相等处理
+        if i < 0 or j < 0 or i >= self.size() or j >= self.size() or i == j:
+            raise IndexError()
+        # 在无向图中，邻接矩阵关于主对角线对称，即满足 (i, j) == (j, i)
+        self.adj_mat[i][j] = 1
+        self.adj_mat[j][i] = 1
+        return
 
 
 """Driver Code"""
 if __name__ == "__main__":
     # 初始化无向图
-    vertices = [1, 3, 2, 5, 4]
-    # 请注意，edges 元素代表顶点索引，即对应 vertices 元素索引
-    edges = [[0, 1], [0, 3], [1, 2], [2, 3], [2, 4], [3, 4]]
-    graph = GraphAdjMat(vertices, edges)
+    # vertices = [1, 3, 2, 5, 4]
+    # # 请注意，edges 元素代表顶点索引，即对应 vertices 元素索引
+    # edges = [[0, 1], [0, 3], [1, 2], [2, 3], [2, 4], [3, 4]]
+    # graph = GraphAdjMat(vertices, edges)
+    graph = GraphAdjMat()
     print("\n初始化后，图为")
-    print(graph)
+    graph.add_node(1) # 0
+    graph.add_node(3) # 1
+    graph.add_node(5) # 2
+
+    graph.add_node(2) # 3
+    graph.add_node(4) # 4
+
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 2)
+    graph.add_edge(0, 3)
+
+    graph.add_edge(1, 2)
+    graph.add_edge(3, 4)
+    graph.add_edge(2, 4)
+    # graph.add_node(2)
+    # graph.remove_node(1)
+    graph.print()
