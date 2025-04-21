3c8a98cc3cb940cc7f6112f91b1316437167895c
huoyinghui <huoyinghui@xizhi-ai.com>
2025-04-21 14:31:08 +0800

    add:bt重复计算

--- codes/python/chapter_dynamic_programming/climbing_stairs_dp_v2.py
+++ codes/python/chapter_dynamic_programming/climbing_stairs_dp_v2.py
@@ -1,8 +1,11 @@
 from itertools import count
+from functools import lru_cache
+
 
 class Solution:
 
     @classmethod
+    @lru_cache()
     def dfs(cls, i: int = 0):
         """
 
@@ -11,12 +14,23 @@ class Solution:
         # 2: 2
         # dp[3] = dp[2] + dp[1] = 3
         # dp[4] = dp[3] + dp[2] = 5
-        #  1       2      3      5     8
+        #  1       2      3      5     8     13
         # dp[1], dp[2], dp[3], dp[4], dp[5], dp[6].... dp[i]
         if i < 3:
             return i
-        count = cls.dfs(i-1) + cls.dfs(i-2)
+        count = cls.dfs(i=i-1) + cls.dfs(i=i-2)
         return count
 
     def climbStairs(self, n: int) -> int:
         return self.dfs(n)
+
+
+def main():
+    n = 6
+    c = Solution().climbStairs(n=n)
+    print(c)
+    pass
+
+
+if __name__ == '__main__':
+    main()
