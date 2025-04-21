652f521955ae4bf543761140979bfe4fbbe35f00
huoyinghui <huoyinghui@xizhi-ai.com>
2025-04-21 10:39:59 +0800

    add:bt重复计算

--- codes/python/chapter_dynamic_programming/demo.py
+++ codes/python/chapter_dynamic_programming/demo.py
@@ -90,9 +90,9 @@ def main():
     """
     给定一个共有 n 阶的楼梯，你每步可以上 1 阶或者2 阶，请问有多少种方案可以爬到楼顶？
     """
-    # ret = solve(38, choice=[1, 2])
+    ret = solve(38, choice=[1, 2])
     # 63245986
-    ret = climbing_stairs_dfs(n=38)
+    # ret = climbing_stairs_dfs(n=38)
     # ret = climbing_stairs_backtrack(n=38)
     print(ret)
     pass
