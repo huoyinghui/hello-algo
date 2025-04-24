15de3ec8a1e78d6610ffaf39f0a147593d56c020
huoyinghui <huoyinghui@xizhi-ai.com>
2025-04-24 10:38:43 +0800

    add:bt重复计算

--- codes/python/chapter_dynamic_programming/climbing_stairs_dp_v2.py
+++ codes/python/chapter_dynamic_programming/climbing_stairs_dp_v2.py
@@ -38,6 +38,9 @@ class Solution:
 
     def minPathSum(self, grid: List[List[int]]) -> int:
         """
+        https://leetcode.cn/problems/0i0mDW/description/
+        给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+        说明：一个机器人每次只能向下或者向右移动一步。
         grid = [
             [1, 3, 1], [1, 5, 1], [4, 2, 1]
         ]
