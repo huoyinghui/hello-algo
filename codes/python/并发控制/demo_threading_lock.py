196fdb372bc46e0d78303236da222c5bcddeb9ba
huoyinghui <huoyinghui@xizhi-ai.com>
2025-09-04 11:55:05 +0800

    update:gil

--- codes/python/并发控制/demo_threading_lock.py
+++ codes/python/并发控制/demo_threading_lock.py
@@ -1,4 +1,6 @@
+import random
 import threading
+import time
 
 lock = threading.Lock()
 counter = 0
@@ -6,16 +8,14 @@ counter = 0
 
 def task():
     global counter
-    for _ in range(100000):
-        # with lock:  # 等价于 lock.acquire() / lock.release()
-        #     counter += 1
-        # lock.acquire()
-        counter += 1
-        # lock.release()
+    time.sleep(random.random() * 0.001)
+    for _ in range(10000):
+        with lock:
+            counter += 1
 
 
 def main():
-    threads = [threading.Thread(target=task) for _ in range(5)]
+    threads = [threading.Thread(target=task) for _ in range(100)]
     for t in threads:
         t.start()
     for t in threads:
